0a0ea80529d9046e9a049fae14f9790344dc9f0a
Philip Vitale <philipmvitale@gmail.com>
2018-09-15 18:27:02 -0400

    Modify line validation for Project03

--- parse_gedcom.py
+++ parse_gedcom.py
@@ -5,29 +5,29 @@ level1 = frozenset(('NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB
 level2 = frozenset(['DATE'])
 
 validTags = [level0, level1, level2]
+noArgumentTags = [frozenset(('HEAD', 'TRLR')), frozenset(('BIRT', 'DEAT', 'MARR', 'DIV'))]
 
-noArgs = [frozenset(('HEAD', 'TRLR')), frozenset(('BIRT', 'DEAT', 'MARR', 'DIV'))]
-
-gedcomFileName = 'proj02test.ged'
+gedcomFileName = 'family.ged'
 gedcomFile = open(gedcomFileName, 'r')
+validatedFile = []
 
 def validateLine(gedcomLine):
     line = gedcomLine.strip().split(' ', 2)
     if len(line) == 0:
-        return '%s|N' % line
+        return 'invalid'
     elif len(line) == 1:
-        return '%s|N' % line[0]
+        return 'invalid'
     elif len(line) == 2:
         tag = line[1]
         try:
             level = int(line[0])
         except ValueError:
-            return '%s|%s|N' % (line[0], tag)
+            return 'invalid'
 
         if level >= 0 and level < len(noArgs) and tag in noArgs[level]:
-            return '%s|%s|Y' % (level, tag)
+            return [level, tag, None]
         else:
-            return '%s|%s|N' % (level, tag)
+            return 'invalid'
     elif len(line) == 3:
         if line[2].strip() == 'INDI' or line[2].strip() == 'FAM':
             tag = line[2]
@@ -36,31 +36,32 @@ def validateLine(gedcomLine):
             tag = line[1]
             arguments = line[2]
             if tag == 'INDI' or tag == 'FAM':
-                return '%s|%s|N|%s' % (line[0], tag, arguments)
+                return 'invalid'
         try:
             level = int(line[0])
         except ValueError:
-            return '%s|%s|N|%s' % (line[0], tag, arguments)
+            return 'invalid'
 
         if level >= 0 and level < len(noArgs) and tag in noArgs[level]:
-            return '%s|%s|N|%s' % (level, tag, arguments)
+            return 'invalid'
 
         if level >= 0 and level < len(validTags) and tag in validTags[level]:
             if tag == 'DATE':
                 try:
                     datetime.datetime.strptime(arguments, '%d %b %Y')
-                    valid = 'Y'
+                    validity = [level, tag, arguments]
                 except ValueError:
-                    valid = 'N'
-                return '%s|%s|%s|%s' % (level, tag, valid, arguments)
+                    validity =  'invalid'
+                return validity
             else:
-                return '%s|%s|Y|%s' % (level, tag, arguments)
+                return [level, tag, arguments]
         else:
-            return '%s|%s|N|%s' % (level, tag, arguments)
+            return 'invalid'
     else:
-        return '%s|N' % line
+        return 'invalid'
 
 for line in gedcomFile:
-    print('--> %s' % line.strip())
-    print('<-- %s' % validateLine(line))
+    validatedLine = validateLine(line)
+    if validatedLine != 'invalid':
+        validatedFile.append(validatedLine)
 
