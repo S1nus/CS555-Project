da1d0ff8f86e7d169e62becb85565d473f0e53df
Philip Vitale <philipmvitale@gmail.com>
2018-09-16 16:03:17 -0400

    Improve file validation algorithm, variable names

--- parse_gedcom.py
+++ parse_gedcom.py
@@ -1,67 +1,59 @@
 import datetime
 
-level0 = frozenset(('INDI', 'FAM', 'HEAD', 'TRLR', 'NOTE'))
-level1 = frozenset(('NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV'))
+level0 = frozenset(['INDI', 'FAM', 'NOTE'])
+level1 = frozenset(['NAME', 'SEX', 'FAMC', 'FAMS', 'HUSB', 'WIFE', 'CHIL'])
 level2 = frozenset(['DATE'])
 
 validTags = [level0, level1, level2]
-noArgumentTags = [frozenset(('HEAD', 'TRLR')), frozenset(('BIRT', 'DEAT', 'MARR', 'DIV'))]
+validLevels = frozenset([0, 1, 2])
 
-gedcomFileName = 'family.ged'
+validTagsNoArgs = [frozenset(['HEAD', 'TRLR']), frozenset(['BIRT', 'DEAT', 'MARR', 'DIV'])]
+validLevelsNoArgs = frozenset([0, 1])
+
+gedcomFileName = 'test.ged'
 gedcomFile = open(gedcomFileName, 'r')
 validatedFile = []
 
-def validateLine(gedcomLine):
-    line = gedcomLine.strip().split(' ', 2)
-    if len(line) == 0:
-        return 'invalid'
-    elif len(line) == 1:
-        return 'invalid'
-    elif len(line) == 2:
-        tag = line[1]
-        try:
-            level = int(line[0])
-        except ValueError:
-            return 'invalid'
+def validateFile(gedcomFile):
+    validatedFile = []
+    
+    for rawLine in gedcomFile:
+        line = rawLine.strip().split(' ', 2)
+        validatedLine = None
+        
+        if len(line) >= 2 and len(line) <= 3:
+            try:
+                level = int(line[0])
+                if len(line) == 2:
+                    tag = line[1]
+                    arguments = None
+                else:
+                    if line[2] == 'INDI' or line[2] == 'FAM':
+                        tag = line[2]
+                        arguments = line[1]
+                    else:
+                        tag = line[1]
+                        arguments = line[2]
+                        if tag == 'INDI' or tag == 'FAM':
+                            raise ValueError
+                
+                if level in validLevelsNoArgs and tag in validTagsNoArgs[level] and not arguments:
+                    validatedLine = [level, tag, arguments]
+                elif level in validLevels and tag in validTags[level]:
+                    if tag == 'DATE':
+                        datetime.datetime.strptime(arguments, '%d %b %Y')
+                        validatedLine = [level, tag, arguments]
+                    else:
+                        validatedLine = [level, tag, arguments]
+                
+                if validatedLine:
+                    validatedFile.append(validatedLine)
+                else:
+                    print('Invalid: %s' % line)
+            except ValueError:
+                print('Invalid: %s' % line)
+        
+    
+    return validatedFile
 
-        if level >= 0 and level < len(noArgumentTags) and tag in noArgumentTags[level]:
-            return [level, tag, None]
-        else:
-            return 'invalid'
-    elif len(line) == 3:
-        if line[2].strip() == 'INDI' or line[2].strip() == 'FAM':
-            tag = line[2]
-            arguments = line[1]
-        else:
-            tag = line[1]
-            arguments = line[2]
-            if tag == 'INDI' or tag == 'FAM':
-                return 'invalid'
-        try:
-            level = int(line[0])
-        except ValueError:
-            return 'invalid'
-
-        if level >= 0 and level < len(noArgumentTags) and tag in noArgumentTags[level]:
-            return 'invalid'
-
-        if level >= 0 and level < len(validTags) and tag in validTags[level]:
-            if tag == 'DATE':
-                try:
-                    datetime.datetime.strptime(arguments, '%d %b %Y')
-                    validity = [level, tag, arguments]
-                except ValueError:
-                    validity =  'invalid'
-                return validity
-            else:
-                return [level, tag, arguments]
-        else:
-            return 'invalid'
-    else:
-        return 'invalid'
-
-for line in gedcomFile:
-    validatedLine = validateLine(line)
-    if validatedLine != 'invalid':
-        validatedFile.append(validatedLine)
-
+validatedFile = validateFile(gedcomFile)
