6f6cfd2b67c7836ac52b5c2b4383632eba88147e
Rahul Arora <rahula.0523@gmail.com>
2018-10-07 15:20:48 -0400

    Added User Story 2 code, tests and ged file

---
+++ scripts/us02.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import gedcom
+def getDate(tag, entry):
+    if entry[tag] != None:
+        return datetime.strptime(entry[tag], '%Y-%m-%d')
+    else:
+        return None
+def birthBeforeMarriage (familyCollection, individualCollection):
+	WrongFamList = []
+	for family in familyCollection:
+		for spouse in individualCollection:
+			if family['Husband ID'] == spouse['ID'] or family['Wife ID'] == spouse['ID']:
+					if getDate('Married', family) < getDate('Birthday', spouse):
+						WrongFamList.append(family['ID'])
+						gedcom.familyError('US02', family['ID'], ('Birthday %s after being married' % spouse['Birthday']))
+	return set(WrongFamList)
+
